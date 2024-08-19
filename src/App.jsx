1b6d2ef4b77e8a0b6326d048c667bde8ceb9777d
fatimawaseem15 <fatimawaseem5000@gmail.com>
2024-08-19 13:22:57 +0500

    Added initial project files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./layout/Navbar";
+import Banner from "./layout/components/Banner";
+import Footer from './layout/Footer';
+import Freebook from './layout/components/freebook';
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Banner />
+      <Freebook />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/components/freebook.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import List from "../../../public/list.json";
+import Slider from "react-slick";  
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Cards from './Cards';
+
+export default function Freebook() {
+    // Filter the data to get only the free books
+    const Filterdata = List.filter(data => data.category === "Free");
+
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+
+    return (
+        <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
+          <div >
+                <h1 className='font-bold py-2'>Free Offered Courses</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eos eius est, dolore non voluptatum, sint minus deserunt sed optio laboriosam repudiandae</p>
+          </div>
+          <Slider {...settings}>
+            {Filterdata.map((item, index) => (
+              <div key={index}>
+                <Cards item={item} />
+              </div>
+            ))}
+          </Slider>
+        </div>
+    );
+}
